f218f6655f5922a387e1f2ffb10dc34b031e578d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-06 20:12:41 +0200

    Added tests to try someting out

---
+++ Tests/PSPGP.Tests.ps1
@@ -0,0 +1,14 @@
+﻿Describe 'PGP Tests' {
+    # prepare things
+    $KeysDirectory = "$env:TEMP\Keys"
+    New-Item -Path $KeysDirectory -Force
+
+    It 'Running New-PGPKey with Username and password should create public and private keys' {
+        New-PGPKey -FilePathPublic $KeysDirectory\PublicPGP.asc -FilePathPrivate $KeysDirectory\PrivatePGP.asc -UserName 'przemyslaw.klys' -Password 'ZielonaMila9!'
+        Test-Path -LiteralPath $KeysDirectory\PublicPGP.asc | Should -Be $true
+        Test-Path -LiteralPath $KeysDirectory\PrivatePGP.asc | Should -Be $true
+    }
+
+    # clean everything
+    Remove-Item -Path $KeysDirectory -Recurse -Force
+}
