44abd47831c22951cb4e49dab8d7441f4c820a61
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-06 20:25:50 +0200

    Update

--- Tests/PSPGP.Tests.ps1
+++ Tests/PSPGP.Tests.ps1
@@ -1,12 +1,14 @@
 ﻿Describe 'PGP Tests' {
     # prepare things
-    $KeysDirectory = "$env:TEMP\Keys"
+    $KeysDirectory = [io.path]::Combine($env:TEMP, 'Keys')
+    $KeyPublic = [io.path]::Combine($KeysDirectory, 'PublicPGP.asc')
+    $KeyPrivate = [io.path]::Combine($KeysDirectory, 'PrivatePGP.asc')
     New-Item -Path $KeysDirectory -Force
 
     It 'Running New-PGPKey with Username and password should create public and private keys' {
-        New-PGPKey -FilePathPublic $KeysDirectory\PublicPGP.asc -FilePathPrivate $KeysDirectory\PrivatePGP.asc -UserName 'przemyslaw.klys' -Password 'ZielonaMila9!'
-        Test-Path -LiteralPath $KeysDirectory\PublicPGP.asc | Should -Be $true
-        Test-Path -LiteralPath $KeysDirectory\PrivatePGP.asc | Should -Be $true
+        New-PGPKey -FilePathPublic $KeyPublic -FilePathPrivate $KeyPrivate -UserName 'przemyslaw.klys' -Password 'ZielonaMila9!'
+        Test-Path -LiteralPath $KeyPublic | Should -Be $true
+        Test-Path -LiteralPath $KeyPrivate | Should -Be $true
     }
 
     # clean everything
