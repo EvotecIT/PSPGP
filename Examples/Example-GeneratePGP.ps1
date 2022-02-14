4b2fda848f80c76bbff6e44cbe46fc4743fe2131
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-02-14 21:06:59 +0100

    Examples #2

--- Examples/Example-GeneratePGP.ps1
+++ Examples/Example-GeneratePGP.ps1
@@ -1,3 +1,4 @@
 ﻿Import-Module .\PSPGP.psd1 -Force
 
-New-PGPKey -FilePathPublic $PSScriptRoot\Keys\PublicPGP.asc -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP.asc -UserName 'przemyslaw.klys' -Password 'ZielonaMila9!'
+New-PGPKey -FilePathPublic $PSScriptRoot\Keys\PublicPGP1.asc -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP1.asc -UserName 'przemyslaw.klys' -Password 'ZielonaMila9!'
+New-PGPKey -FilePathPublic $PSScriptRoot\Keys\PublicPGP2.asc -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP2.asc -UserName 'przemyslaw.klys' -Password 'ZielonaMila10!'
