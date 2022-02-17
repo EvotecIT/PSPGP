b13a61790a21f41dce1d1070f16022619dabdadd
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-02-17 23:21:54 +0100

    Update example

--- Examples/Example-EncryptFolderMultipleKeys.ps1
+++ Examples/Example-EncryptFolderMultipleKeys.ps1
@@ -1,3 +1,5 @@
 ﻿Import-Module .\PSPGP.psd1 -Force
 
 Protect-PGP -FilePathPublic @("$PSScriptRoot\Keys\PublicPGP.asc", "$PSScriptRoot\Keys\PublicPGP2.asc") -FolderPath $PSScriptRoot\Test -OutputFolderPath $PSScriptRoot\Encoded
+
+Protect-PGP -FilePathPublic @("$PSScriptRoot\Keys\PublicPGP.asc", "$PSScriptRoot\Keys\PublicPGP2.asc") -FolderPath '~\Downloads\Transfer' -OutputFolderPath '~\Downloads\Transfer'
