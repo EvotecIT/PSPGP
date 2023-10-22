f747002acc02a4d8c4dd8832806f86c8ca77fb68
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-10-22 10:17:32 +0200

    Update module builder to newest version

--- Publish/Manage-PSPGP.ps1
+++ Publish/Manage-PSPGP.ps1
@@ -1,170 +1,131 @@
 ﻿Clear-Host
-Import-Module 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSPublishModule\PSPublishModule.psd1' -Force
 
-$Configuration = @{
-    Information = @{
-        ModuleName           = 'PSPGP'
+Build-Module -ModuleName 'PSPGP' {
+    # Usual defaults as per standard module
+    $Manifest = [ordered] @{
+        # Minimum version of the Windows PowerShell engine required by this module
+        PowerShellVersion      = '5.1'
+        # prevent using over CORE/PS 7
+        CompatiblePSEditions   = @('Desktop', 'Core')
+        # ID used to uniquely identify this module
+        GUID                   = 'edbf6d52-2d66-405e-a4d4-d4a95db8fb45'
+        # Version number of this module.
+        ModuleVersion          = '0.1.X'
+        # Author of this module
+        Author                 = 'Przemyslaw Klys'
+        # Company or vendor of this module
+        CompanyName            = 'Evotec'
+        # Copyright statement for this module
+        Copyright              = "(c) 2011 - $((Get-Date).Year) Przemyslaw Klys @ Evotec. All rights reserved."
+        # Description of the functionality provided by this module
+        Description            = 'PSPGP is a PowerShell module that provides PGP functionality in PowerShell. It allows encrypting and decrypting files/folders and strings using PGP.'
+        # Tags applied to this module. These help with module discovery in online galleries.
+        Tags                   = @('pgp', 'gpg', 'encrypt', 'decrypt', 'windows', 'macos', 'linux')
+        # A URL to the main website for this project.
+        ProjectUri             = 'https://github.com/EvotecIT/PSPGP'
 
-        DirectoryProjects    = 'C:\Support\GitHub'
-        DirectoryModulesCore = "$Env:USERPROFILE\Documents\PowerShell\Modules"
-        DirectoryModules     = "$Env:USERPROFILE\Documents\WindowsPowerShell\Modules"
+        IconUri                = 'https://evotec.xyz/wp-content/uploads/2021/08/PSPGP.png'
 
-        FunctionsToExport    = 'Public'
-        AliasesToExport      = 'Public'
+        LicenseUri             = 'https://github.com/EvotecIT/PSPGP/blob/master/License'
+        DotNetFrameworkVersion = '4.7.2'
+    }
+    New-ConfigurationManifest @Manifest
+    # Add external module dependencies, using loop for simplicity
+    New-ConfigurationModule -Type ExternalModule -Name 'Microsoft.PowerShell.Management', 'Microsoft.PowerShell.Utility'
 
-        LibrariesCore        = 'Lib\Standard'
-        LibrariesDefault     = 'Lib\Standard'
+    # Add approved modules, that can be used as a dependency, but only when specific function from those modules is used
+    # And on that time only that function and dependant functions will be copied over
+    # Keep in mind it has it's limits when "copying" functions such as it should not depend on DLLs or other external files
+    New-ConfigurationModule -Type ApprovedModule -Name 'PSSharedGoods', 'PSWriteColor', 'Connectimo', 'PSUnifi', 'PSWebToolbox', 'PSMyPassword'
 
-        Manifest             = @{
-            # Minimum version of the Windows PowerShell engine required by this module
-            PowerShellVersion    = '5.1'
-            # prevent using over CORE/PS 7
-            CompatiblePSEditions = @('Desktop', 'Core')
-            # ID used to uniquely identify this module
-            GUID                 = 'edbf6d52-2d66-405e-a4d4-d4a95db8fb45'
-            # Version number of this module.
-            ModuleVersion        = '0.1.X'
-            # Author of this module
-            Author               = 'Przemyslaw Klys'
-            # Company or vendor of this module
-            CompanyName          = 'Evotec'
-            # Copyright statement for this module
-            Copyright            = "(c) 2011 - $((Get-Date).Year) Przemyslaw Klys @ Evotec. All rights reserved."
-            # Description of the functionality provided by this module
-            Description          = 'PSPGP is a PowerShell module that provides PGP functionality in PowerShell. It allows encrypting and decrypting files/folders and strings using PGP.'
-            # Tags applied to this module. These help with module discovery in online galleries.
-            Tags                 = @('pgp', 'gpg', 'encrypt', 'decrypt', 'windows', 'macos', 'linux')
-            # A URL to the main website for this project.
-            ProjectUri           = 'https://github.com/EvotecIT/PSPGP'
+    $ConfigurationFormat = [ordered] @{
+        RemoveComments                              = $false
 
-            IconUri              = 'https://evotec.xyz/wp-content/uploads/2021/08/PSPGP.png'
+        PlaceOpenBraceEnable                        = $true
+        PlaceOpenBraceOnSameLine                    = $true
+        PlaceOpenBraceNewLineAfter                  = $true
+        PlaceOpenBraceIgnoreOneLineBlock            = $false
 
-            #LicenseUri           = 'https://github.com/EvotecIT/PSWriteWord/blob/master/License'
+        PlaceCloseBraceEnable                       = $true
+        PlaceCloseBraceNewLineAfter                 = $true
+        PlaceCloseBraceIgnoreOneLineBlock           = $false
+        PlaceCloseBraceNoEmptyLineBefore            = $true
 
-            RequiredModules      = @(
-                #@{ ModuleName = 'PSSharedGoods'; ModuleVersion = "Latest"; Guid = 'ee272aa8-baaa-4edf-9f45-b6d6f7d844fe' }
-            )
+        UseConsistentIndentationEnable              = $true
+        UseConsistentIndentationKind                = 'space'
+        UseConsistentIndentationPipelineIndentation = 'IncreaseIndentationAfterEveryPipeline'
+        UseConsistentIndentationIndentationSize     = 4
+
+        UseConsistentWhitespaceEnable               = $true
+        UseConsistentWhitespaceCheckInnerBrace      = $true
+        UseConsistentWhitespaceCheckOpenBrace       = $true
+        UseConsistentWhitespaceCheckOpenParen       = $true
+        UseConsistentWhitespaceCheckOperator        = $true
+        UseConsistentWhitespaceCheckPipe            = $true
+        UseConsistentWhitespaceCheckSeparator       = $true
+
+        AlignAssignmentStatementEnable              = $true
+        AlignAssignmentStatementCheckHashtable      = $true
+
+        UseCorrectCasingEnable                      = $true
+    }
+    # format PSD1 and PSM1 files when merging into a single file
+    # enable formatting is not required as Configuration is provided
+    New-ConfigurationFormat -ApplyTo 'OnMergePSM1', 'OnMergePSD1' -Sort None @ConfigurationFormat
+    # format PSD1 and PSM1 files within the module
+    # enable formatting is required to make sure that formatting is applied (with default settings)
+    New-ConfigurationFormat -ApplyTo 'DefaultPSD1', 'DefaultPSM1' -EnableFormatting -Sort None
+    # when creating PSD1 use special style without comments and with only required parameters
+    New-ConfigurationFormat -ApplyTo 'DefaultPSD1', 'OnMergePSD1' -PSD1Style 'Minimal'
+
+    # configuration for documentation, at the same time it enables documentation processing
+    New-ConfigurationDocumentation -Enable:$false -StartClean -UpdateWhenNew -PathReadme 'Docs\Readme.md' -Path 'Docs'
+
+    New-ConfigurationImportModule -ImportSelf #-ImportRequiredModules
+
+    $newConfigurationBuildSplat = @{
+        Enable                            = $true
+        SignModule                        = $true
+        MergeModuleOnBuild                = $true
+        MergeFunctionsFromApprovedModules = $true
+        CertificateThumbprint             = '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703'
+        DotSourceLibraries                = $false
+        DotSourceClasses                  = $false
+        SeparateFileLibraries             = $true
+        DeleteTargetModuleBeforeBuild     = $true
+    }
+
+    New-ConfigurationBuild @newConfigurationBuildSplat  #-DotSourceLibraries -DotSourceClasses -MergeModuleOnBuild -Enable -SignModule -DeleteTargetModuleBeforeBuild -CertificateThumbprint '483292C9E317AA13B07BB7A96AE9D1A5ED9E7703' -MergeFunctionsFromApprovedModules
+
+    $newConfigurationArtefactSplat = @{
+        Type                = 'Unpacked'
+        Enable              = $true
+        Path                = "$PSScriptRoot\..\Artefacts\Unpacked"
+        ModulesPath         = "$PSScriptRoot\..\Artefacts\Unpacked\Modules"
+        RequiredModulesPath = "$PSScriptRoot\..\Artefacts\Unpacked\Modules"
+        AddRequiredModules  = $true
+        CopyFiles           = @{
+            #"Examples\PublishingExample\Example-ExchangeEssentials.ps1" = "RunMe.ps1"
         }
     }
-    Options     = @{
-        Merge             = @{
-            Sort           = 'None'
-            FormatCodePSM1 = @{
-                Enabled           = $true
-                RemoveComments    = $true
-                FormatterSettings = @{
-                    IncludeRules = @(
-                        'PSPlaceOpenBrace',
-                        'PSPlaceCloseBrace',
-                        'PSUseConsistentWhitespace',
-                        'PSUseConsistentIndentation',
-                        'PSAlignAssignmentStatement',
-                        'PSUseCorrectCasing'
-                    )
-
-                    Rules        = @{
-                        PSPlaceOpenBrace           = @{
-                            Enable             = $true
-                            OnSameLine         = $true
-                            NewLineAfter       = $true
-                            IgnoreOneLineBlock = $true
-                        }
-
-                        PSPlaceCloseBrace          = @{
-                            Enable             = $true
-                            NewLineAfter       = $false
-                            IgnoreOneLineBlock = $true
-                            NoEmptyLineBefore  = $false
-                        }
-
-                        PSUseConsistentIndentation = @{
-                            Enable              = $true
-                            Kind                = 'space'
-                            PipelineIndentation = 'IncreaseIndentationAfterEveryPipeline'
-                            IndentationSize     = 4
-                        }
-
-                        PSUseConsistentWhitespace  = @{
-                            Enable          = $true
-                            CheckInnerBrace = $true
-                            CheckOpenBrace  = $true
-                            CheckOpenParen  = $true
-                            CheckOperator   = $true
-                            CheckPipe       = $true
-                            CheckSeparator  = $true
-                        }
-
-                        PSAlignAssignmentStatement = @{
-                            Enable         = $true
-                            CheckHashtable = $true
-                        }
-
-                        PSUseCorrectCasing         = @{
-                            Enable = $true
-                        }
-                    }
-                }
-            }
-            FormatCodePSD1 = @{
-                Enabled        = $true
-                RemoveComments = $false
-            }
-            Integrate      = @{
-                ApprovedModules = @('PSSharedGoods', 'PSWriteColor', 'Connectimo', 'PSUnifi', 'PSWebToolbox', 'PSMyPassword')
-            }
+    New-ConfigurationArtefact @newConfigurationArtefactSplat -CopyFilesRelative
+    $newConfigurationArtefactSplat = @{
+        Type                = 'Packed'
+        Enable              = $true
+        Path                = "$PSScriptRoot\..\Artefacts\Packed"
+        ModulesPath         = "$PSScriptRoot\..\Artefacts\Packed\Modules"
+        RequiredModulesPath = "$PSScriptRoot\..\Artefacts\Packed\Modules"
+        AddRequiredModules  = $true
+        CopyFiles           = @{
+            #"Examples\PublishingExample\Example-ExchangeEssentials.ps1" = "RunMe.ps1"
         }
-        Standard          = @{
-            FormatCodePSM1 = @{
-
-            }
-            FormatCodePSD1 = @{
-                Enabled = $true
-                #RemoveComments = $true
-            }
-        }
-        PowerShellGallery = @{
-            ApiKey   = 'C:\Support\Important\PowerShellGalleryAPI.txt'
-            FromFile = $true
-        }
-        GitHub            = @{
-            ApiKey   = 'C:\Support\Important\GithubAPI.txt'
-            FromFile = $true
-            UserName = 'EvotecIT'
-            #RepositoryName = 'PSWriteHTML'
-        }
-        Documentation     = @{
-            Path       = 'Docs'
-            PathReadme = 'Docs\Readme.md'
-        }
+        ArtefactName        = '<ModuleName>.v<ModuleVersion>.zip'
     }
-    Steps       = @{
-        BuildModule        = @{  # requires Enable to be on to process all of that
-            Enable              = $true
-            DeleteBefore        = $true
-            Merge               = $true
-            MergeMissing        = $true
-            LibrarySeparateFile = $true
-            LibraryDotSource    = $false
-            ClassesDotSource    = $false
-            SignMerged          = $true
-            CreateFileCatalog   = $false # not working
-            Releases            = $true
-            ReleasesUnpacked    = $false
-            RefreshPSD1Only     = $false
-        }
-        BuildDocumentation = $false
-        ImportModules      = @{
-            Self            = $true
-            RequiredModules = $false
-            Verbose         = $false
-        }
-        PublishModule      = @{  # requires Enable to be on to process all of that
-            Enabled      = $true
-            Prerelease   = ''
-            RequireForce = $false
-            GitHub       = $true
-        }
-    }
-}
+    New-ConfigurationArtefact @newConfigurationArtefactSplat
 
-New-PrepareModule -Configuration $Configuration
+    New-ConfigurationTest -TestsPath "$PSScriptRoot\..\Tests" -Enable
+
+    # global options for publishing to github/psgallery
+    #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
+    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
+} -ExitCode
