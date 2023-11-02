5db771336bb7caf0415098e14bd80689afe8c3f4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-11-02 21:12:58 +0100

    cleanup sources c#

--- Sources/PSPGP/OnImportAndRemove.cs
+++ Sources/PSPGP/OnImportAndRemove.cs
@@ -17,36 +17,8 @@ public class OnModuleImportAndRemove : IModuleAssemblyInitializer, IModuleAssemb
     }
 
     private static Assembly MyResolveEventHandler(object sender, ResolveEventArgs args) {
-        // These are known to be problematic in .NET Framework, force it to use our packaged dlls.
-        if (args.Name.StartsWith("System.Memory,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Memory.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Runtime.CompilerServices.Unsafe,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Runtime.CompilerServices.Unsafe.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Numerics.Vectors,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Numerics.Vectors.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Drawing.Common,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Drawing.Common.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Buffers,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Buffers.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.ValueTuple,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.ValueTuple.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("System.Text.Encoding.CodePages,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "System.Text.Encoding.CodePages.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("BouncyCastle.Cryptography,")) {
+        if (args.Name.StartsWith("BouncyCastle.Cryptography,")) {
             string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "BouncyCastle.Cryptography.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("Newtonsoft.Json,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "Newtonsoft.Json.dll");
-            return Assembly.LoadFile(binPath);
-        } else if (args.Name.StartsWith("Microsoft.Identity.Client,")) {
-            string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "Microsoft.Identity.Client.dll");
             return Assembly.LoadFile(binPath);
         } else if (args.Name.StartsWith("PgpCore,")) {
             string binPath = Path.Combine(Path.GetDirectoryName(typeof(OnModuleImportAndRemove).Assembly.Location), "PgpCore.dll");
