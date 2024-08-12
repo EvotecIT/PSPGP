65332619431e5ca087ae372145f5f22d427a3291
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-12 15:33:24 +0200

    Wokrs

--- Examples/Example-EncryptFolderMultipleKeys.ps1
+++ Examples/Example-EncryptFolderMultipleKeys.ps1
@@ -2,4 +2,4 @@
 
 Protect-PGP -FilePathPublic @("$PSScriptRoot\Keys\PublicPGP.asc", "$PSScriptRoot\Keys\PublicPGP2.asc") -FolderPath $PSScriptRoot\Test -OutputFolderPath $PSScriptRoot\Encoded
 
-Protect-PGP -FilePathPublic @("$PSScriptRoot\Keys\PublicPGP.asc", "$PSScriptRoot\Keys\PublicPGP2.asc") -FolderPath '~\Downloads\Transfer' -OutputFolderPath '~\Downloads\Transfer'
+Protect-PGP -FilePathPublic @("$PSScriptRoot\Keys\PublicPGP.asc", "$PSScriptRoot\Keys\PublicPGP2.asc") -FolderPath '~\Downloads\Cloudflare' -OutputFolderPath '~\Downloads\Cloudflare'
