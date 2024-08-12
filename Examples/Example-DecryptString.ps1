b45fb7d841745872c3b2bb43a4c91dba47efdfa1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-12 14:23:42 +0200

    Works

--- Examples/Example-DecryptString.ps1
+++ Examples/Example-DecryptString.ps1
@@ -1,5 +1,5 @@
 ﻿Import-Module .\PSPGP.psd1 -Force
 
-$ProtectedString = Protect-PGP -FilePathPublic $PSScriptRoot\Keys\PublicPGP.asc -String "This is string to encrypt"
+$ProtectedString = Protect-PGP -FilePathPublic "$PSScriptRoot\Keys\PublicPGP1.asc" -String "This is string to encrypt"
 
-Unprotect-PGP -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP.asc -Password 'ZielonaMila9!' -String $ProtectedString
+Unprotect-PGP -FilePathPrivate "$PSScriptRoot\Keys\PrivatePGP1.asc" -Password 'ZielonaMila9!' -String $ProtectedString
