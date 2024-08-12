405c33e368dd72bbe90bef017d85df7e656498b3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-12 15:33:06 +0200

    works

--- Examples/Example-DecryptFolder.ps1
+++ Examples/Example-DecryptFolder.ps1
@@ -1,3 +1,3 @@
 ﻿Import-Module .\PSPGP.psd1 -Force
 
-Unprotect-PGP -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP.asc -Password 'ZielonaMila9!' -FolderPath $PSScriptRoot\Encoded -OutputFolderPath $PSScriptRoot\Decoded
+Unprotect-PGP -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP1.asc -Password 'ZielonaMila9!' -FolderPath $PSScriptRoot\Encoded -OutputFolderPath $PSScriptRoot\Decoded
--- Examples/Example-EncryptFolder.ps1
+++ Examples/Example-EncryptFolder.ps1
@@ -1,3 +1,3 @@
 ﻿Import-Module .\PSPGP.psd1 -Force
 
-Protect-PGP -FilePathPublic $PSScriptRoot\Keys\PublicPGP.asc -FolderPath $PSScriptRoot\Test -OutputFolderPath $PSScriptRoot\Encoded
+Protect-PGP -FilePathPublic $PSScriptRoot\Keys\PublicPGP1.asc -FolderPath $PSScriptRoot\Test -OutputFolderPath $PSScriptRoot\Encoded 
