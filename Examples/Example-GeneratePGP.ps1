8bb37e12390ea63db82651b7d1f783c09d414231
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-18 14:39:12 +0200

    update example

--- Examples/Example-GeneratePGP.ps1
+++ Examples/Example-GeneratePGP.ps1
@@ -4,7 +4,8 @@ New-PGPKey -HashAlgorithm Sha512 -FilePathPublic $PSScriptRoot\Keys\PublicPGP1.a
 New-PGPKey -FilePathPublic $PSScriptRoot\Keys\PublicPGP2.asc -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP2.asc -Password 'ZielonaMila10!' -UserName 'przemyslaw.klys@evotec.pl'
 
 # No password is required if you don't want one and just trust the key
-New-PGPKey -FilePathPublic $PSScriptRoot\Keys\PublicPGP3.asc -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP3.asc -Strength 4096 -Certainty 8
+#New-PGPKey -FilePathPublic $PSScriptRoot\Keys\PublicPGP3.asc -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP3.asc -Strength 4096 -Certainty 8
+New-PGPKey -HashAlgorithm Sha512 -FilePathPublic $PSScriptRoot\Keys\PublicPGP3.asc -FilePathPrivate $PSScriptRoot\Keys\PrivatePGP3.asc
 
 <#
 New-PGPKey -FilePathPublic '\\ad1\c$\Temp\PublicPGP1.asc' -FilePathPrivate '\\ad1\c$\Temp\PrivatePGP1.asc' -UserName 'przemyslaw.klys' -Password 'ZielonaMila9!'
