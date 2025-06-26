d5070cea0c1c4604a9983c30d6408a1a5bc72841
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-06-26 18:57:08 +0200

    docs: clarify Test-PGP example results

--- Examples/Example-VerifySignature.ps1
+++ Examples/Example-VerifySignature.ps1
@@ -3,5 +3,7 @@
 $ProtectedString = Protect-PGP -FilePathPublic $PSScriptRoot\Keys\PublicPGP.asc -String "This is string to encrypt"
 
 Test-PGP -FilePathPublic $PSScriptRoot\Keys\PublicPGP.asc -String $ProtectedString
+# Expected to produce no output when signature is valid
 
 Test-PGP -FilePathPublic $PSScriptRoot\Keys\PublicPGP.asc -FolderPath $PSScriptRoot\Encoded
+# Returns objects with Status $true for valid signatures
